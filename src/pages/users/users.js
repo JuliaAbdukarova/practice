35e6a51a9722b90aa068a378d56695aa4b28e1ea
Timur A <atimtim@yandex.ru>
2023-10-18 16:30:56 +0300

    Устранение ошибки расположения данныъ о пользовталях на стренице users

--- src/pages/users/users.js
+++ src/pages/users/users.js
@@ -49,25 +49,26 @@ const UsersContainer = ({className}) => {
 
     return (
         <PrivateContent access={[ROLE.ADMIN]} serverError = {errorMessage}>
-            <div className = {className}></div>
-            <H2>Пользователи</H2>
-            <div>
-                <TableRow>
-                    <div className = "login-column" >Логин</div>
-                    <div className = "registered-at-column" >Дата регистрации</div>
-                    <div className = "role-column">Роль</div>
-                </TableRow>
-                    {users.map(({id, login, registeredAt, roleId})=>(
-                        <UserRow
-                            key={id}
-                            id={id}
-                            login={login}
-                            registeredAt={registeredAt}
-                            roleId={roleId}
-                            roles={roles.filter(({id: roleId})=> roleId !== ROLE.GUEST)}
-                            onUserRemove={()=>onUserRemove(id)}
-                        />
-                    ))}
+            <div className = {className}>
+                <H2>Пользователи</H2>
+                <div>
+                    <TableRow>
+                        <div className = "login-column" >Логин</div>
+                        <div className = "registered-at-column" >Дата регистрации</div>
+                        <div className = "role-column">Роль</div>
+                    </TableRow>
+                        {users.map(({id, login, registeredAt, roleId})=>(
+                            <UserRow
+                                key={id}
+                                id={id}
+                                login={login}
+                                registeredAt={registeredAt}
+                                roleId={roleId}
+                                roles={roles.filter(({id: roleId})=> roleId !== ROLE.GUEST)}
+                                onUserRemove={()=>onUserRemove(id)}
+                            />
+                        ))}
+                </div>
             </div>
         </PrivateContent>
     );
